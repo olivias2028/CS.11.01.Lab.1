d8977756d6708ed79d407e052be5fbf648a47268
Olivia <olivias2028@student.cis.edu.hk>
2025-09-13 14:11:44 +0800

    Olivia Song

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        System.out.println("What is your age?"); //Prompt users to enter their age
+        Scanner scanner = new Scanner(System.in); //Create a scanner that receives user inputs
+        int age = scanner.nextInt(); //Assign the value scanned into integer box named "age"
+        if (age<0) { //Removes invalid inputs such as being over 100 years old or under 0
+            System.out.println("Invalid Input!");
+            System.exit(0);
+        }
+        else
+        {
+            age = age/2 +7; //Calculate
+            System.out.println("The youngest you should date is a " + age + " year old."); //Print output
     }
-}
+}}
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
-    }
-}
+        System.out.println("What is your age?"); //Prompt users to enter their age
+        Scanner scanner = new Scanner(System.in); //Create a scanner that receives user inputs
+        int age = scanner.nextInt(); //Assign the value scanned into integer box named "age"
+        if (age>100 || age<0) { //Removes invalid inputs such as being over 100 years old or under 0
+            System.out.println("Invalid Input!");
+            System.exit(0);
+        }
+        else
+        {
+            age = 100-age; //Calculate
+            System.out.println("Before giving up on a book, you should read at least " + age + " pages."); //Print output
+        }
+    }}
